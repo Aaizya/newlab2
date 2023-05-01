65d15ac6533fe651f7d15b14bec6b0f6a44a8821
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 21:38:09 +0600

    assignment3 #17

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -4,7 +4,19 @@ import java.util.NoSuchElementException;
 public class MyArrayListQueue<T> {
     private MyArrayList<T> queue;
 
-    public void enqueue(T data) // This funct add data
+    public void enqueue(T data) // This function add data
     {
-        queue.add();
+        queue.add(data);
+    }
+    public boolean isEmpty()   // This function checks, is queue empty and also  returns true or false
+    {
+        return queue.isEmpty();
+    }
+    public T dequeue ()   // This function removes and  also returns the front element of the queue
+    {
+        if (isEmpty())
+        {
+            throw new NoSuchElementException();
+        }
+        return queue.remove(0);
     }
