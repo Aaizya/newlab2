b1869dfe05ad6b9a7e2f5f44bff57c66907858ed
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 19:59:32 +0600

    assignment3  #3

--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -11,3 +11,6 @@ public class MyLinkedListStack<T> {
     public void pushMethod(T data) // This function adds an element to the top of the stack.
     {
         stack.addFirst(data);
+    }
+
+    public boolean isEmpty() // This function checks, is stack empty and returns true or false
