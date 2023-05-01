91c74b2412fa6d541ec763c8b8eb1cf32bcb4abf
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 21:26:10 +0600

    assignment3 #16

---
+++ MyArrayListQueue.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class MyArrayListQueue<T> {
+    private MyArrayList<T> queue;
+
+    public void enqueue(T data) // This funct add data
+    {
+        queue.add();
+    }
