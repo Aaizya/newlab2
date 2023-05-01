0b18917ad74ea8c849f14467886ba5845c881d30
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 22:24:52 +0600

    assignment3 #20

---
+++ MyArrayListStack.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class MyArrayListStack<T> {
+    private ArrayList<T> stack;
+
+    // Constructor initializes an empty ArrayList stack
+    public MyArrayListStack() {
+        stack = new ArrayList<>();
+    }
