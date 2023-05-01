a347bb46039b8c9a7057c160dfc28b42d052f9e0
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 20:47:27 +0600

    assignment3 #12

--- MyLinkedListQueue.java
+++ MyLinkedListQueue.java
@@ -2,4 +2,19 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class MyLinkedListQueue<E> {
-    private LinkedList<E> queue; // instance variable of type LinkedList 
+    private LinkedList<E> queue; // instance variable of type LinkedList
+    public MyLinkedListQueue() {
+        queue = new LinkedList<>(); // initializing the LinkedList instance variable
+    }
+    // Removes and returns the front element of the queue using addLast method
+    public void enqueue( E element) {
+        queue.add(element);
+    }
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new NosuchElementException();
+        }
+        return queue.remove();
+      }
+    }
+}
