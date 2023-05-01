ba98aa7d3ad3ec961ec5b14af49c9a9543c2bcbe
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 19:53:51 +0600

    assignment3 #2

--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -4,3 +4,9 @@ import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
     private LinkedList<T> stack;
     public MyLinkedListStack()
+    {
+        stack = new LinkedList<T>();
+    }
+
+    public void pushMethod(T data) // This function adds an element to the top of the stack.
+    {
