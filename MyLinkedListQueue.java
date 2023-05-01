fc35f51129fa30b9f892cdb7d3f9ac445d5710d3
Aruzhan <aruzhan1471@gmail.com>
2023-05-01 20:26:57 +0600

    assignment3 #12

---
+++ MyLinkedListQueue.java
@@ -0,0 +1,5 @@
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class MyLinkedListQueue<E> {
+    private LinkedList<E> queue; // instance variable of type LinkedList 
